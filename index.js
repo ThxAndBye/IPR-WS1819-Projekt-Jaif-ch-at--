801f7aefc6e5b71e17dfe74f75b514c90ee05f50
ThxAndBye <gpg@thxandbye.de>
2018-12-28 21:49:21 +0100

    socket io server now checks for url, sends the url only as a new message, client can recieve url's

--- index.js
+++ index.js
@@ -13,10 +13,34 @@ app.get('/styles.css', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+
     io.emit('chat message', msg);
+
+    //message contains url
+    let expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    let regex = new RegExp(expression);
+
+    if(expression.test(msg)) {
+      url = msg.match(expression);
+
+      msg = JSON.parse(msg);
+      fullmsg = { "author": msg.author , "message": addhttp(url) };
+      fullmsg = JSON.stringify(fullmsg);
+      
+      io.emit('url', fullmsg);
+    }
+
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+function addhttp(url) {
+  if (!/^(f|ht)tps?:\/\//i.test(url)) {
+     url = "http://" + url;
+  }
+  return url;
+}
